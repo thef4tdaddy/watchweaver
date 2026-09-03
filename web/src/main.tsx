0ad5def0a3ebd04d6507cd5810f78cc3f11fda5e
thef4tdaddy <github@f4tdaddy.com>
2026-09-03 15:13:21 -0500

    fix: load Serializd checkpoint theme

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './network-boundary.css'
+import './serializd-theme.css'
 import SetupGate from './SetupGate.tsx'
 
 createRoot(document.getElementById('root')!).render(
